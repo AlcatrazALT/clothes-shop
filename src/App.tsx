225b05c4c44f798365e16b8104dcf98ee80dba72
Oleg Mazalevsky <olegmazalevsky@gmail.com>
2023-03-11 15:54:05 +0700

    feat: add mockup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { Guid } from 'typescript-guid';
+
+import { Category } from 'types';
+
 const App = () => {
-  return <div>Hello World</div>;
+  const categories: Category[] = [
+    { id: Guid.create(), title: 'Hats' },
+    { id: Guid.create(), title: 'Jackets' },
+    { id: Guid.create(), title: 'Sneakers' },
+    { id: Guid.create(), title: 'Women' },
+    { id: Guid.create(), title: 'Men' },
+  ];
+
+  return (
+    <div className='categoriesContainer'>
+      {categories.map((category) => (
+        <div key={category.id.toString()} className='categoryContainer'>
+          <div className='categoryBackground'></div>
+          <div className='categoryBodyContainer'>
+            <h2>{category.title}</h2>
+            <p>Show Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
---
+++ src/types/category.ts
@@ -0,0 +1,6 @@
+import { Guid } from 'typescript-guid';
+
+export interface Category {
+  id: Guid;
+  title: string;
+}
